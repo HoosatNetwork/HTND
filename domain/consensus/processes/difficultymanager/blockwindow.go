450ba559cd6141064bdcbe9a9c1a5a0d80c56611
Tonto <tonto@winblows>
2026-02-19 12:53:24 +0200

    Revert block window.

--- domain/consensus/processes/difficultymanager/blockwindow.go
+++ domain/consensus/processes/difficultymanager/blockwindow.go
@@ -19,7 +19,12 @@ type difficultyBlock struct {
 type blockWindow []difficultyBlock
 
 func (dm *difficultyManager) getDifficultyBlock(
-	header externalapi.BlockHeader, blockHash *externalapi.DomainHash) (difficultyBlock, error) {
+	stagingArea *model.StagingArea, blockHash *externalapi.DomainHash) (difficultyBlock, error) {
+
+	header, err := dm.headerStore.BlockHeader(dm.databaseContext, stagingArea, blockHash)
+	if err != nil {
+		return difficultyBlock{}, err
+	}
 	return difficultyBlock{
 		timeInMilliseconds: header.TimeInMilliseconds(),
 		Bits:               header.Bits(),
@@ -32,54 +37,22 @@ func (dm *difficultyManager) getDifficultyBlock(
 // blocks in the past of startingNode, the sorting is unspecified.
 // If the number of blocks in the past of startingNode is less then windowSize,
 // the window will be padded by genesis blocks to achieve a size of windowSize.
-func (dm *difficultyManager) blockWindow(
-	stagingArea *model.StagingArea,
-	startingNode *externalapi.DomainHash,
-	windowSize int,
-) (blockWindow, []*externalapi.DomainHash, error) {
+func (dm *difficultyManager) blockWindow(stagingArea *model.StagingArea, startingNode *externalapi.DomainHash, windowSize int) (blockWindow,
+	[]*externalapi.DomainHash, error) {
 
+	window := make(blockWindow, 0, windowSize)
 	windowHashes, err := dm.dagTraversalManager.BlockWindow(stagingArea, startingNode, windowSize)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	if len(windowHashes) == 0 {
-		return make(blockWindow, 0), windowHashes, nil
-	}
-
-	// OPTIMIZATION: Batch retrieve all headers at once instead of one by one
-	// This reduces database round trips from N to 1, significantly improving performance
-	headers, err := dm.headerStore.BlockHeaders(dm.databaseContext, stagingArea, windowHashes)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	// Build the window from the batch-retrieved headers
-	window := make(blockWindow, 0, len(headers))
-	for i, header := range headers {
-		block, err := dm.getDifficultyBlock(header, windowHashes[i])
+	for _, hash := range windowHashes {
+		block, err := dm.getDifficultyBlock(stagingArea, hash)
 		if err != nil {
 			return nil, nil, err
 		}
 		window = append(window, block)
 	}
-
-	// Check if the last header has DAAScore == 43334187
-	// Note: This maintains the original behavior where the last header is processed twice
-	// (once in the loop above and once here), which may be intentional for difficulty calculation
-	if len(windowHashes) > 0 {
-		lastHash := windowHashes[len(windowHashes)-1]
-		lastHeader := headers[len(headers)-1]
-		if lastHeader.DAAScore() <= 43334187 {
-			lastBlock, err := dm.getDifficultyBlock(lastHeader, lastHash)
-			if err != nil {
-				return nil, nil, err
-			}
-			singleWindow := blockWindow{lastBlock}
-			return singleWindow, []*externalapi.DomainHash{lastHash}, nil
-		}
-	}
-
 	return window, windowHashes, nil
 }
 
