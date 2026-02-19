69090c41e5568d5447dfe943df73415d5f449f0d
Tonto <tonto@winblows>
2026-02-19 12:45:07 +0200

    Update main to respect GO env variables.

--- main.go
+++ main.go
@@ -5,22 +5,48 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"runtime"
+	"runtime/debug"
+	"strconv"
 
 	"github.com/Hoosat-Oy/HTND/app"
 )
 
+func getEnvInt(key string, defaultVal int) int64 {
+	if v := os.Getenv(key); v != "" {
+		if n, err := strconv.Atoi(v); err == nil && n > 0 {
+			return int64(n)
+		}
+	}
+	return int64(defaultVal)
+}
+
+func getEnvStr(key string, defaultVal string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return defaultVal
+}
+
+func init() {
+	os.Setenv("GOGC", getEnvStr("GOGC", "200"))
+	debug.SetMemoryLimit(getEnvInt("GOMEMLIMIT", 14_000_000_000))
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU()) // Set the maximum number of CPUs that can be executing simultaneously
-	//debug.SetGCPercent(200)              // Run GoGC at 200% for less frequent garbage collection
-	// debug.SetMemoryLimit(8_000_000_000) // Set memory soft limit to 16GB
-	//runtime.SetBlockProfileRate(1)     // Set block profile rate to 1 to enable block profiling
-	//runtime.SetMutexProfileFraction(1) // Set mutex profile fraction to 1 to enable mutex profiling
-	// go func() {
-	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
-	// }()
+	if os.Getenv("HTND_PROFILER") != "" {
+		runtime.SetBlockProfileRate(1)     // Set block profile rate to 1 to enable block profiling
+		runtime.SetMutexProfileFraction(1) // Set mutex profile fraction to 1 to enable mutex profiling
+		go func() {
+			log.Println(http.ListenAndServe("127.0.0.1:6060", nil))
+		}()
+	}
+
 	if err := app.StartApp(); err != nil {
 		os.Exit(1)
 	}
