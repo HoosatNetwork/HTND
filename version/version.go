f75e5b524f53e20cd149c7b019b316d3739b6e3b
Tonto <tonto@winblows>
2026-02-22 17:52:55 +0200

    For RC release.

--- version/version.go
+++ version/version.go
@@ -17,7 +17,7 @@ const (
 // appBuild is defined as a variable so it can be overridden during the build
 // process with '-ldflags "-X github.com/Hoosat-Oy/HTND/version.appBuild=foo"' if needed.
 // It MUST only contain characters from validCharacters.
-var appBuild string = ""
+var appBuild string = "RC1"
 
 var version = "" // string used for memoization of version
 
