8e9131a27ed7430eeb11c1a14148824a0ba8777c
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-02-25 09:02:48 +0200

    Update generate not to create grpc files, but vtproto only

--- infrastructure/network/netadapter/server/grpcserver/protowire/generate.go
+++ infrastructure/network/netadapter/server/grpcserver/protowire/generate.go
@@ -1,3 +1,3 @@
-//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative --go-vtproto_out=. --go-vtproto_opt=paths=source_relative,pool=truesize=true p2p.proto rpc.proto messages.proto
+//go:generate protoc --go_out=. --go_opt=paths=source_relative  --go-vtproto_out=. --go-vtproto_opt=paths=source_relative,pool=truesize=true p2p.proto rpc.proto messages.proto
 
 package protowire
