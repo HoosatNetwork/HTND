83881c9f8926fa48d6a34c1d576c5d849ae3a172
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-03-30 10:26:38 +0300

    Fix two impossible comparisons as uint32 can't be below 0.

--- app/rpc/rpchandlers/get_utxos_by_addresses.go
+++ app/rpc/rpchandlers/get_utxos_by_addresses.go
@@ -43,7 +43,7 @@ func HandleGetUTXOsByAddresses(context *rpccontext.Context, _ *router.Router, re
 
 	getUTXOsByAddressesRequest := request.(*appmessage.GetUTXOsByAddressesRequestMessage)
 
-	if getUTXOsByAddressesRequest.Limit < 0 || getUTXOsByAddressesRequest.Limit > 10_000_000 {
+	if getUTXOsByAddressesRequest.Limit > 10_000_000 {
 		errorMessage := &appmessage.GetUTXOsByAddressesResponseMessage{}
 		errorMessage.Error = appmessage.RPCErrorf("Invalid limit: %d. Limit must be between 0 and 10,000,000", getUTXOsByAddressesRequest.Limit)
 		return errorMessage, nil
