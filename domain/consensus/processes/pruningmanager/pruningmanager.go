975efe9b938b0071ea9696def12bc8500586eeac
MattF42 <MattF42@github.com>
2026-04-05 13:25:58 +0100

    Revert "Actually pay attention to deletion-depth flag!"
    This reverts commit 68c8ba6c592f4d1c62dd6a08db928efce0053f10.
    
    It needs more work that this....

--- domain/consensus/processes/pruningmanager/pruningmanager.go
+++ domain/consensus/processes/pruningmanager/pruningmanager.go
@@ -1112,13 +1112,9 @@ func (pm *pruningManager) updatePruningPoint() error {
 		}
 	}
 	log.Infof("Deletion of past blocks")
-	// Actually pay attention to deletion-depth flag!
-	shouldDelete, effectivePruningPoint := pm.CheckIfShouldDeletePastBlocks(stagingArea, pruningPoint)
-    if shouldDelete {
-       err = pm.deletePastBlocks(stagingArea, effectivePruningPoint)
-	   if err != nil {
-		   return err
-	   }
+	err = pm.deletePastBlocks(stagingArea, pruningPoint)
+	if err != nil {
+		return err
 	}
 
 	log.Info("Commit all changes")
