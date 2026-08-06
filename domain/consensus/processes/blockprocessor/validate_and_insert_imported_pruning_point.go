fae82ff4891b6f60124a8da15e7a748bce67be97
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-08-07 00:02:10 +0300

    Actually check if the pruning points are in valid chain.

--- domain/consensus/processes/blockprocessor/validate_and_insert_imported_pruning_point.go
+++ domain/consensus/processes/blockprocessor/validate_and_insert_imported_pruning_point.go
@@ -22,13 +22,13 @@ func (bp *blockProcessor) validateAndInsertImportedPruningPoint(
 		return errors.Wrapf(ruleerrors.ErrUnexpectedPruningPoint, "%s is not a valid pruning point", newPruningPointHash)
 	}
 
-	// arePruningPointsInValidChain, err := bp.pruningManager.ArePruningPointsInValidChain(stagingArea)
-	// if err != nil {
-	// 	return err
-	// }
-	// if !arePruningPointsInValidChain {
-	// 	return errors.Wrapf(ruleerrors.ErrInvalidPruningPointsChain, "pruning points do not compose a valid chain to genesis")
-	// }
+	arePruningPointsInValidChain, err := bp.pruningManager.ArePruningPointsInValidChain(stagingArea)
+	if err != nil {
+		return err
+	}
+	if !arePruningPointsInValidChain {
+		return errors.Wrapf(ruleerrors.ErrInvalidPruningPointsChain, "pruning points do not compose a valid chain to genesis")
+	}
 
 	log.Infof("Updating consensus state manager according to the new pruning point %s", newPruningPointHash)
 	err = bp.consensusStateManager.ImportPruningPointUTXOSet(stagingArea, newPruningPointHash)
