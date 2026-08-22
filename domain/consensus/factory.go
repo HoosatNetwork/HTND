e761aa23b8d97cc9fc6662ff12f57423f17bda73
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-08-22 14:19:00 +0300

    Increase UTXODiff LRU cache size, as restorePastUTXO traverses quite often, quite many UTXODiffs.

--- domain/consensus/factory.go
+++ domain/consensus/factory.go
@@ -188,7 +188,7 @@ func (f *factory) NewConsensus(config *Config, db infrastructuredatabase.Databas
 	blockStatusStore := blockstatusstore.New(prefixBucket, pruningWindowSizePlusFinalityDepthForCache, preallocateCaches)
 	multisetStore := multisetstore.New(prefixBucket, 1000, preallocateCaches)
 	pruningStore := pruningstore.New(prefixBucket, 2, preallocateCaches)
-	utxoDiffStore := utxodiffstore.New(prefixBucket, 1000, preallocateCaches)
+	utxoDiffStore := utxodiffstore.New(prefixBucket, 250_000, preallocateCaches)
 	consensusStateStore := consensusstatestore.New(prefixBucket, 50_000, preallocateCaches)
 
 	headersSelectedTipStore := headersselectedtipstore.New(prefixBucket)
