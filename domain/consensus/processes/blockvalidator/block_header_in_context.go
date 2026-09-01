9c1e158044d31d8d4da08ac89b416cf946f1b2f6
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-09-01 20:53:21 +0300

    Revert "Fix IBD validation failures due to incomplete reachability data"
    This reverts commit becd6eb6d5b92148c8092547331ef0ee48d681d5.

--- domain/consensus/processes/blockvalidator/block_header_in_context.go
+++ domain/consensus/processes/blockvalidator/block_header_in_context.go
@@ -64,12 +64,32 @@ func (v *blockValidator) ValidateHeaderInContext(stagingArea *model.StagingArea,
 		}
 	}
 
-	// Now enabled: checkIndirectParents with BuildParents fixed to handle IBD scenarios
+	// During IBD, validate only direct parents since BuildParents may return incomplete
+	// parent sets due to missing DAG topology data. Full indirect parents validation
+	// requires complete reachability data.
 	if !isBlockWithTrustedData {
-		err = v.checkIndirectParents(stagingArea, header)
-		if err != nil {
-			return err
+		// Check only direct parents (level 0) during IBD
+		if len(header.Parents()) > 0 {
+			expectedParents, err := v.blockParentBuilder.BuildParents(stagingArea, header.DAAScore(), header.DirectParents(), false)
+			if err != nil {
+				return err
+			}
+			
+			// Only validate level 0 (direct parents) during IBD
+			if len(expectedParents) > 0 {
+				areDirectParentsEqual := header.Parents()[0].Equal(expectedParents[0])
+				if !areDirectParentsEqual {
+					return errors.Wrapf(ruleerrors.ErrUnexpectedParents, "unexpected direct block parents")
+				}
+			} else {
+				return errors.Wrapf(ruleerrors.ErrUnexpectedParents, "expected parents is empty")
+			}
 		}
+		// TODO: Enable full indirect parents validation after fixing IBD reachability issues
+		// err = v.checkIndirectParents(stagingArea, header)
+		// if err != nil {
+		// 	return err
+		// }
 	}
 
 	err = v.mergeDepthManager.CheckBoundedMergeDepth(stagingArea, blockHash, ghostdagData, header, isBlockWithTrustedData)
@@ -84,8 +104,9 @@ func (v *blockValidator) ValidateHeaderInContext(stagingArea *model.StagingArea,
 	}
 
 	if !isBlockWithTrustedData {
-		// During IBD, use a lenient DAA score check that handles missing DAA score data
-		err = v.checkDAAScoreWithFallback(stagingArea, blockHash, header)
+		// During IBD, use a more lenient DAA score threshold since DAA score calculation
+		// may be slightly inaccurate due to incomplete DAG data
+		err = v.checkDAAScoreWithThreshold(stagingArea, blockHash, header, 100) // Larger threshold for IBD
 		if err != nil {
 			return err
 		}
@@ -274,24 +295,14 @@ func (v *blockValidator) checkDAAScore(stagingArea *model.StagingArea, blockHash
 	return nil
 }
 
-// checkDAAScoreWithFallback checks DAA score with fallback for missing data during IBD
-func (v *blockValidator) checkDAAScoreWithFallback(stagingArea *model.StagingArea, blockHash *externalapi.DomainHash,
-	header externalapi.BlockHeader,
+// checkDAAScoreWithThreshold checks DAA score with a custom threshold for IBD
+func (v *blockValidator) checkDAAScoreWithThreshold(stagingArea *model.StagingArea, blockHash *externalapi.DomainHash,
+	header externalapi.BlockHeader, threshold uint64,
 ) error {
 	expectedDAAScore, err := v.daaBlocksStore.DAAScore(v.databaseContext, stagingArea, blockHash)
 	if err != nil {
-		// During IBD, DAA score might not be available yet. Skip the check in this case.
-		// The header's DAA score comes from a trusted peer, so we can accept it.
-		if database.IsNotFoundError(err) {
-			log.Debugf("Skipping DAA score check for %s - DAA score not found (IBD scenario): %v", blockHash, err)
-			return nil
-		}
 		return err
 	}
-	
-	// Use a lenient threshold during IBD to account for potential calculation inaccuracies
-	// due to incomplete DAG data
-	var threshold uint64 = 100
 	if header.DAAScore()+threshold < expectedDAAScore {
 		return errors.Wrapf(ruleerrors.ErrUnexpectedDAAScore, "block DAA score of %d is not the expected value of %d (threshold: %d)", header.DAAScore(), expectedDAAScore, threshold)
 	}
