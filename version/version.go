ace480940b7a98b9680e96b6794949c1d1f8e2fd
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2026-09-13 20:52:18 +0300

    pump(version) 2.17.1 -> 2.17.2

--- version/version.go
+++ version/version.go
@@ -12,7 +12,7 @@ const validCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs
 const (
 	appMajor uint = 2
 	appMinor uint = 17
-	appPatch uint = 1
+	appPatch uint = 2
 )
 
 // shortCommitLength is the number of hex digits kept from the full commit hash
